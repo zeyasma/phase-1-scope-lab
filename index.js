156f024db04573e98e0af794a96866eb289da586
Zeinab Mohamed <zeinabmohameddaud1812@gmail.com>
2026-09-22 20:07:56 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newName) {
+  bestCustomer = newName;
+}
+
+const leastFavoriteCustomer = "bob";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "not bob";
+}
